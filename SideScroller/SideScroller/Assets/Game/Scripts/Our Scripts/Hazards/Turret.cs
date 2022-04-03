278d2e7db56e67407a07c8b6b31176290fa0c7db
tbolivar <58379057+tbolivar@users.noreply.github.com>
2022-04-03 00:16:58 -0400

    Turrets Shoot if Player is Within Set Distance
    -- adjusted guitar projectile prefab's colliders
    -- added player-to-turret distance float value (set in inspector per turret script) for x and y axis
    -- if player is within this range, the turret will shoot & play shooting sound, otherwise turret will not shoot

--- SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/Hazards/Turret.cs
+++ SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/Hazards/Turret.cs
@@ -15,21 +15,35 @@ public class Turret : MonoBehaviour
     [SerializeField]
     private float projectileLifeTime = 5.0f;
 
+    [SerializeField]
+    private float playerDistanceStartShootingX = 65.0f;
+    [SerializeField]
+    private float playerDistanceStartShootingY = 65.0f;
+
     private Vector3 shootDirection;
 
     private ObjectPoolManager.PoolTypes poolType;
 
+    private GameObject player;
+    private bool shooting;
+
     // Audio
-    public AudioSource turretSource;
+    public AudioSource turretSource = null;
 
     private void Start()
     {
         // Audio
-        turretSource = GetComponent<AudioSource>();
-        if (turretSource == null)
+        if (!TryGetComponent<AudioSource>(out turretSource))
         {
             // only disabling this for the time being for testing
             //Debug.LogError("No AudioSource found");
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null)
+        {
+            Debug.LogError("Null Player in Turret script");
         }
 
         shootDirection = fireLocation.position - transform.position;
@@ -49,31 +63,54 @@ public class Turret : MonoBehaviour
             poolType = ObjectPoolManager.PoolTypes.YELLOWTURRET;
         }
 
+        shooting = false;
         StartCoroutine(ShootProjectile());
+    }
+
+    private void Update()
+    {
+        float distanceX = Mathf.Abs(gameObject.transform.position.x - player.transform.position.x);
+        float distanceY = Mathf.Abs(gameObject.transform.position.y - player.transform.position.y);
+
+        if (!shooting && (distanceX <= playerDistanceStartShootingX && distanceY <= playerDistanceStartShootingY))
+        {
+            shooting = true;
+        }
+        else if (shooting && (distanceX > playerDistanceStartShootingX || distanceY > playerDistanceStartShootingY))
+        {
+            shooting = false;
+        }
     }
 
     private IEnumerator ShootProjectile()
     {
-        yield return new WaitForSeconds(fireRate);
-
-        GameObject projectileGO = ObjectPoolManager.Instance.GetPooledObject(poolType);
-
-        if (projectileGO != null)
+        if (shooting)
         {
-            HazardProjectile hazardProjectile = projectileGO.GetComponent<HazardProjectile>();
-            hazardProjectile.transform.position = fireLocation.position;
-            hazardProjectile.LifeTime = projectileLifeTime;
-            hazardProjectile.Speed = projectileSpeed;
+            yield return new WaitForSeconds(fireRate);
 
-            projectileGO.SetActive(true);
-            hazardProjectile.Fire(shootDirection);
+            GameObject projectileGO = ObjectPoolManager.Instance.GetPooledObject(poolType);
 
-            // Audio
-            PlayShootingSound();
+            if (projectileGO != null)
+            {
+                HazardProjectile hazardProjectile = projectileGO.GetComponent<HazardProjectile>();
+                hazardProjectile.transform.position = fireLocation.position;
+                hazardProjectile.LifeTime = projectileLifeTime;
+                hazardProjectile.Speed = projectileSpeed;
+
+                projectileGO.SetActive(true);
+                hazardProjectile.Fire(shootDirection);
+
+                // Audio
+                PlayShootingSound();
+            }
+            else
+            {
+                Debug.Log("Null Projectile");
+            }
         }
         else
         {
-            Debug.Log("Null Projectile");
+            yield return new WaitForSeconds(0.1f);
         }
 
         StartCoroutine(ShootProjectile());
