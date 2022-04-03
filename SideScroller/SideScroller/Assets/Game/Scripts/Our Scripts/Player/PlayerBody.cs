801476004a990807acf61302abaf1e0e82f61c63
ExtraTurbo <78029339+ExtraTurbo@users.noreply.github.com>
2022-04-03 01:42:52 -0400

    late nite jammin'
    90% of sound effects are implemented

--- SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/Player/PlayerBody.cs
+++ SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/Player/PlayerBody.cs
@@ -34,6 +34,8 @@ public class PlayerBody : MonoBehaviour
     [SerializeField]
     private Material beamMaterial;
 
+    private AudioSource guitarSource = null;
+
     private bool guitarAvailable;
     private bool shieldAvailable;
     public bool ShieldAvailable
@@ -96,6 +98,7 @@ public class PlayerBody : MonoBehaviour
     private IEnumerator GuitarAtk()
     {
         guitarAvailable = false;
+        //guitarSource.PlayOneShot(guitarSource.clip);
 
         Vector3 bulletStartPos = fireLocation.transform.position;
 
