9dae2e60a549f6dafe79f7e754f594249fe2cf6a
tbolivar <58379057+tbolivar@users.noreply.github.com>
2022-04-03 20:50:09 -0400

    Added a Script for Boxes

--- SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/RedBox.cs
+++ SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/RedBox.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class RedBox : MonoBehaviour
 {
+    private AudioSource audioSource;
+
     private MeshRenderer r;
     private BoxCollider c;
+    private GameObject child;
 
+    private void Awake()
+    {
+        TryGetComponent<AudioSource>(out audioSource);
+        TryGetComponent<MeshRenderer>(out r);
+        TryGetComponent<BoxCollider>(out c);
+        child = transform.GetChild(0).gameObject;
+    }
+
+    public void DestroyBox()
+    {
+        StartCoroutine(Death());
+    }
+
+    private IEnumerator Death()
+    {
+        if (r != null)
+        {
+            r.enabled = false;
+        }
+        if (c != null)
+        {
+            c.enabled = false;
+        }
+        if (child != null)
+        {
+            child.SetActive(false);
+        }
+        if (audioSource != null)
+        {
+            //audioSource.PlayOneShot("");
+        }
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
 }
