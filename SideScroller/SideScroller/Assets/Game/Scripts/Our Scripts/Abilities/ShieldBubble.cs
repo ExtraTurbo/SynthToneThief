5c7a545ba143ffa4dfbae80f3034cefabfe9ba81
tbolivar <58379057+tbolivar@users.noreply.github.com>
2022-04-15 21:07:09 -0400

    Shield Plays Sound When Destroyed
    -- plays sound when shield wears off/is destroyed
    -- placeholder sound atm, can be changed in Shield prefab's ShieldBubble component
    -- adjusted collider of Spikes prefab

--- SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/Abilities/ShieldBubble.cs
+++ SideScroller/SideScroller/Assets/Game/Scripts/Our Scripts/Abilities/ShieldBubble.cs
@@ -11,8 +11,36 @@ public class ShieldBubble : MonoBehaviour
         get { return lifeTime; }
     }
 
-    void OnEnable()
+    [SerializeField]
+    private AudioClip shieldOnClip;
+    [SerializeField]
+    private AudioClip shieldOffClip;
+
+    private AudioSource audioSource;
+
+    private CapsuleCollider c;
+    private MeshRenderer mesh;
+
+    private bool destroying = false;
+
+    private void Start()
     {
+        TryGetComponent<AudioSource>(out audioSource);
+        TryGetComponent<CapsuleCollider>(out c);
+        TryGetComponent<MeshRenderer>(out mesh);
+    }
+
+    private void OnEnable()
+    {
+        if (mesh != null)
+        {
+            mesh.enabled = true;
+        }
+        if (c != null)
+        {
+            c.enabled = true;
+        }
+        
         StartCoroutine(UptimeCountdown());
     }
 
@@ -21,26 +49,48 @@ public class ShieldBubble : MonoBehaviour
         if (other.CompareTag("Shieldable"))
         {
             other.gameObject.SetActive(false);
-            StartCoroutine(PopShield());
+            DestroyShield();
         }
         else if (other.CompareTag("Spikes"))
         {
             other.gameObject.GetComponent<Spikes>().SetSpikeDmg();
-            StartCoroutine(PopShield());
+            DestroyShield();
         }
     }
 
     private IEnumerator UptimeCountdown()
     {
         yield return new WaitForSeconds(lifeTime);
-        gameObject.SetActive(false);
-        StopAllCoroutines();
+        DestroyShield();
     }
 
     private IEnumerator PopShield()
     {
         yield return new WaitForSeconds(0.05f);
         gameObject.SetActive(false);
+        destroying = false;
         StopAllCoroutines();
+    }
+
+    public void DestroyShield()
+    {
+        StartCoroutine(ShieldDestruction());
+    }
+
+    private IEnumerator ShieldDestruction()
+    {
+        if (!destroying)
+        {
+            destroying = true;
+            if (audioSource != null && shieldOffClip != null)
+            {
+                mesh.enabled = false;
+                c.enabled = false;
+                audioSource.PlayOneShot(shieldOffClip, 0.8f);
+                yield return new WaitForSeconds(0.3f);
+            }
+            StartCoroutine(PopShield());
+        }
+        yield return null;
     }
 }
